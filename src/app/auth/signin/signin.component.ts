79f8914d539628a27d55f31aa2539f02563f54fa
Akwasi Kusi-Appiah <akwasi.kusi-appiah@amalitech.com>
2025-08-08 00:41:08 +0000

    change sign in page

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -12,8 +12,9 @@ import { AuthService } from '../../config/services/authService/auth-service.serv
   templateUrl: './signin.component.html',
    styles: [`
     .form-input {
-      @apply w-full px-4 py-2 mb-3 border rounded border-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-300;
+      @apply w-full px-4 py-2 border rounded border-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-400;
     }
+
     .btn-primary {
       @apply bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700;
     }
