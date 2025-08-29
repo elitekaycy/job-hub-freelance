4c8bc22b3e2e6267640cac0fce9e4193c9914d4e
elitekaycy <dicksonanyaele1234@gmail.com>
2025-08-29 11:12:12 +0000

    fix: environment staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
+    production: true,
     userPoolId: 'eu-central-1_j2EOkhirZ',
     userPoolClientId: '1f5ubm02dgkk3f9p0g65vancp5', 
     baseURL:"https://3c0fiwy8m9.execute-api.eu-central-1.amazonaws.com/staging-test" 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    userPoolId: 'eu-central-1_vsDdYLv0W',
-    userPoolClientId: '4dptii3aj0p9u2sc0jge4fvfhg',  
-    baseURL:"https://hc4mdt2ga4.execute-api.eu-central-1.amazonaws.com/dev"
+    userPoolId: 'eu-central-1_j2EOkhirZ',
+    userPoolClientId: '1f5ubm02dgkk3f9p0g65vancp5', 
+    baseURL:"https://3c0fiwy8m9.execute-api.eu-central-1.amazonaws.com/staging-test" 
 };
